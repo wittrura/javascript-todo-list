aac16270477ed7750d8e25abcc7ee039f90a8383
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-23 16:49:44 -0500

    Set up listeners for editing and deleting

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
   // model
   model = [
     {taskName: 'Do some stuff', taskComplete: false},
-    {taskName: 'Eat healthy', taskComplete: true},
-    {taskName: 'Get to bed early', taskComplete: true}
+    {taskName: 'Eat healthy', taskComplete: false},
+    {taskName: 'Get to bed early', taskComplete: false},
+    {taskName: 'Bring lunch to school', taskComplete: true}
   ];
 
   // controller
@@ -26,6 +27,16 @@
       return newTask;
     },
 
+    completeTask: function(event) {
+      console.log('completed task');
+      // console.log('re-rendered task list');
+    },
+
+    editTask: function(event) {
+      console.log('edited task');
+      // console.log('re-rendered task list');
+    },
+
     init: function() {
       view.init();
     }
@@ -33,31 +44,38 @@
 
   // view
   view = {
-    renderTasks: function(tasks) {
+    cleanTasks: function() {
       // clear out task list
       let taskList = document.getElementById('taskList');
       taskList.innerHTML = '';
       // clear typed in values in input box
       document.getElementsByTagName('input')[0].value = '';
+    },
+
+    renderTasks: function(tasks) {
+      this.cleanTasks();
 
       tasks.forEach(function(task) {
-        // crete a new li for each task
-        let taskElement = document.createElement('li');
-        taskElement.textContent = task.taskName;
-        taskList.appendChild(taskElement);
+        // only show components which are NOT complete
+        if (!task.taskComplete) {
+          // create a new li for each task
+          let taskElement = document.createElement('li');
+          taskElement.textContent = task.taskName;
+          taskList.appendChild(taskElement);
 
-        // add glyphicon for editing
-        let taskEdit = document.createElement('span');
-        taskEdit.textContent = 'EDIT';
-        taskEdit.style.paddingRight = '10px';
-        taskEdit.style.float = 'left';
-        taskElement.appendChild(taskEdit);
+          // add glyphicon for editing
+          let taskEdit = document.createElement('span');
+          taskEdit.textContent = 'EDIT';
+          taskEdit.style.paddingRight = '10px';
+          taskEdit.style.float = 'left';
+          taskElement.appendChild(taskEdit);
 
-        // add glyphicon for marking complete
-        let taskComplete = document.createElement('span');
-        taskComplete.textContent = 'X';
-        taskComplete.style.float = 'right';
-        taskElement.appendChild(taskComplete);
+          // add glyphicon for marking complete
+          let taskComplete = document.createElement('span');
+          taskComplete.textContent = 'X';
+          taskComplete.style.float = 'right';
+          taskElement.appendChild(taskComplete);
+        }
       });
 
     },
@@ -66,6 +84,14 @@
       this.renderTasks(controller.getTasks());
 
       document.getElementById('add_todo').addEventListener('click', controller.createTask);
+
+      document.getElementById('taskList').addEventListener('click', function(e) {
+        console.log('clicked to update' + e.target.textContent);
+      });
+
+      document.getElementById('taskList').addEventListener('click', function(e) {
+        console.log('clicked to delete' + e.target.textContent);
+      });
     }
   };
 
